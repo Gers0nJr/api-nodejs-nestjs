1131b0100f3ac9bfcbf4dbb992f8c1d6a0f3fab0
Gers0nJr <gersonkenedy@hotmail.com>
2020-12-10 14:51:55 -0300

    Authentication passaport

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -10,3 +10,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
+
+/**https://medium.com/@iago.maiasilva/construindo-uma-api-com-nestjs-postgresql-e-docker-parte-1-criando-nosso-primeiro-endpoint-248d4b8ecc9c */
+/**https://medium.com/@shaibenshimol/nestjs-and-mysql-in-10-minutes-711e02ec1dab */
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { ReturnUserDto } from './dtos/return-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Role } from '../auth/role.decorator';
+import { UserRole } from './user-roles.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  async createAdminUser(@Body() createUserDto: CreateUserDto,): Promise<ReturnUserDto> {
+  @Role(UserRole.ADMIN)
+  @UseGuards(AuthGuard(), RolesGuard)
+  async createAdminUser(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<ReturnUserDto> {
     const user = await this.usersService.createAdminUser(createUserDto);
     return {
       user,
